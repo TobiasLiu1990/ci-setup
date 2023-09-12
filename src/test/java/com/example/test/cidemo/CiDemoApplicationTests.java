6c62e9b01c04277455bbb6f4f871bca6f68b920f
TobiasLiu1990 <tobias.liu@live.se>
2023-09-12 08:41:46 +0200

    x

---
+++ src/test/java/com/example/test/cidemo/CiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
